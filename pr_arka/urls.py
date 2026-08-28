158654304548790d0d836df303bd936ced59c9c6
sebastian <juansebastianinga76@gmail.com>
2026-08-28 09:11:18 -0500

    cambios2

--- pr_arka/urls.py
+++ pr_arka/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_sebas.urls')),
+    path('registro/', include('app_sebas.urls')),
+    path('chat/', include('app_chat.urls')),
 ]
