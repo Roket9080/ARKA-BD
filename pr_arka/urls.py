e3b8231cc06fbb320af42e92a3ca3baee08f27a3
Miguelito90 <migueherazo2004@gmail.com>
2026-08-28 11:44:23 -0500

    Configurar urls principales con prefijos para todas las apps

--- pr_arka/urls.py
+++ pr_arka/urls.py
@@ -4,12 +4,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_miguel.urls')),
-    path('', include('app_Gomez.urls')),
-    path('', include('app_Liseth.urls')),
-    path('', include("app_Alejandro_07.urls")),
-    path('', include('app_cristian.urls')),
-    path('', include('app_martinez.urls')),
+    path('miguel/', include('app_miguel.urls')),
+    path('santiago/', include('app_Gomez.urls')),
+    path('liseth/', include('app_Liseth.urls')),
+    path('alejandro/', include("app_Alejandro_07.urls")),
+    path('cristian/', include('app_cristian.urls')),
+    path('martinez/', include('app_martinez.urls')),
     path('registro/', include('app_sebas.urls')),
     path('chat/', include('app_chat.urls')),
     
